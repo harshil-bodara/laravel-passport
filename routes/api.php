bf41bf0499300839868b8379f7badcd9b5deef91
harshil-bodara <developer.harshilbodara@gmail.com>
2022-01-12 01:04:02 +0530

    create a new app

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Controllers
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\UsersController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,20 +16,4 @@ use App\Http\Controllers\API\UsersController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-Route::group(['middleware' => ['cors', 'json.response']], function () {
-    Route::post('/user/login', [AuthController::class, 'login']);
-    Route::post('/user/register', [AuthController::class, 'register']);
-    Route::post('/user/lost-password', [AuthController::class, 'sendResetLinkResponse']);
-    Route::post('/user/reset-password', [AuthController::class, 'sendResetResponse']);
-    Route::get('/user/verify-email/{id}', [AuthController::class, 'verify']);
-});
-
-Route::middleware('auth:api')->group(function () {
-    Route::post('/user/logout', [AuthController::class, 'logout']);
-
-    // user controller
-    Route::resource('user', UsersController::class);
 });
